cdfe1811ba89c8d45e5f0dce0d9d3dfe801d18e4
mrtee <tungfint@gmail.com>
2026-05-28 08:16:36 +0700

    add custom next.js server for cPanel

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const hostname = "0.0.0.0";
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
